5fd16f8267a92c560c0c266f8675fa067b207263
JCamiloo <jcamilo.osorio15@gmail.com>
2020-11-21 23:28:26 -0500

    init project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+require('./database/config').dbConnection();
+
+const app = express();
+
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+  if (err) throw new Error(err);
+
+  console.log('Server running on port', process.env.PORT);
+});
