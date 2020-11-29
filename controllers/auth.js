3decfe512fbe5b1b769838c9738e5e6d8c282cfa
JCamiloo <jcamilo.osorio15@gmail.com>
2020-11-29 12:23:33 -0500

    using bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,35 @@
 const { response } = require("express");
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    const emailExist = await User.findOne({ email });
 
-  res.json({
-    success: true,
-    message: 'Create user'
-  });
+    if (emailExist) {
+      return res.status(400).json({
+        success: false,
+        message: 'email already taken'
+      })
+    }
+
+    const user = new User(req.body);
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.json({
+      success: true,
+      body: user
+    });
+  } catch(error) {
+    res.status(500).json({
+      success: true,
+      message: 'Create user'
+    });
+  }
 }
 
 module.exports = { createUser };
